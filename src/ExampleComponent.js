a4207f9438cc44cce78fe390eb413b3e5a998494
duruchima <duruchima1@gmail.com>
2020-05-09 18:17:07 -0400

    Done.

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -9,6 +9,13 @@ class ExampleComponent extends Component {
         <p>Whoa!</p>
       </div>
     );
+    return (
+      <div className="example">
+        <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="a deeply impressed man, perhaps thinking 'whoa'"/>
+        <p>Whoa!</p>
+      </div>
+    );
+  
   }
 }
 
